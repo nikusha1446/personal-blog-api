5ef87377cf554aeeb5ecc3d0cd33bc17c6f135a9
Nikusha <nikusha1446@gmail.com>
2025-08-30 20:16:52 +0400

    chore: set up Prisma ORM

---
+++ prisma/migrations/20250830154900_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "public"."Article" (
+    "id" UUID NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "tags" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
